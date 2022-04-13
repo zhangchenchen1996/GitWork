cc6d40981735b290072ad117e128c94e6024fe1a
zhangchenchen <chenchen371481@163.com>
2022-04-13 15:18:50 +0800

    second commit

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello Git");
         System.out.println("hello Git2");
+        System.out.println("hello Git3 master");
     }
 }
